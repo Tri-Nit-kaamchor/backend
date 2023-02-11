c1384c2a134d8c1b4431f02db6008251b00fc19e
spykid-111 <rrsingh1101@gmail.com>
2023-02-11 21:38:02 +0530

    :sparkles: QR Generator added

---
+++ qrCode.js
@@ -0,0 +1,11 @@
+const QRCode = require('qrcode')
+
+function qrGenerator(url) {
+    QRCode.toFile('./qrCode.png', url, {type: 'png'} , (err) => {
+    if(err) throw err;
+        console.log("QR generated!")
+    })
+}
+module.exports = function (url) {
+    qrGenerator(url)
+}
---
+++ test.js
@@ -0,0 +1,3 @@
+const qrGenerator = require('./qrCode')
+
+qrGenerator('google.com')
